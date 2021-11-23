31661129f2953f3896cbca59a8c1c87b8bda3de5
Jordath <sonofthefrozenmoon@yahoo.com>
2021-11-22 17:40:49 -0700

    app reset

--- backend/api/restaurants.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import RecipesCtrl from "./recipes.controller.js"
-
-const router = express.Router()
-
-router.route("/").get(RecipesCtrl.apiGetRecipes)
-
-export default router
